ea83e464d9453eec086e3db09aab68481d453693
cepg3 <2133165@etu.cchic.ca>
2024-03-12 15:22:06 -0400

    Chore : Redo folder structure

---
+++ bug_buster/src/components/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import logo from "./logo.svg";
+import "../assets/css/App.css";
+
+function App() {
+	return (
+		<div className="App">
+			<header className="App-header">ALLLO</header>
+		</div>
+	);
+}
+
+export default App;
