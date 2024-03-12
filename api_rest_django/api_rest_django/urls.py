ab988400bb8ab25e3e911dd26c86689fcf1681ba
cepg3 <2133165@etu.cchic.ca>
2024-03-12 14:55:13 -0400

    Feat : Register router

--- api_rest_django/api_rest_django/urls.py
+++ api_rest_django/api_rest_django/urls.py
@@ -1,22 +1,21 @@
-"""
-URL configuration for api_rest_django project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path
+from api import views
+from rest_framework import routers
+
+router = routers.DefaultRouter()
+router.register(r'cards', views.CardViewSet)
+router.register(r'bugcards', views.BugCardViewSet)
+router.register(r'usercards', views.UserCardViewSet)
+router.register(r'startcards', views.StartCardViewSet)
+router.register(r'middlecards', views.MiddleCardViewSet)
+router.register(r'endcards', views.EndCardViewSet)
+router.register(r'players', views.PlayerViewSet)
+router.register(r'games', views.GameViewSet)
+router.register(r'leaderboardpoints', views.LeaderBoardPointsViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
+
+urlpatterns += router.urls
