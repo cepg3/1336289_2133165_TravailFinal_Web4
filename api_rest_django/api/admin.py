52e33fbd4d546c9b93d58de76a223212d1de21d6
cepg3 <2133165@etu.cchic.ca>
2024-03-12 14:54:19 -0400

    Feat : Add models to administration page

--- api_rest_django/api/admin.py
+++ api_rest_django/api/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from api import models
 
-# Register your models here.
+admin.site.register(models.Card)
+admin.site.register(models.BugCard)
+admin.site.register(models.UserCard)
+admin.site.register(models.StartCard)
+admin.site.register(models.MiddleCard)
+admin.site.register(models.EndCard)
+admin.site.register(models.Player)
+admin.site.register(models.Game)
+admin.site.register(models.LeaderBoardPoints)
