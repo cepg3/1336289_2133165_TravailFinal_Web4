380bb263ea3d07b1bda5a8bf9e859649126d035f
cepg3 <2133165@etu.cchic.ca>
2024-03-12 13:10:51 -0400

    Feat : Start the API REST with Django

--- api_rest_django/api/models.py
+++ api_rest_django/api/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+class Player(models.Model):
+    """
+    Represents a player in the game. 
+    The Players can only join one game at a time. 
+    There is no verification for the username, so it is possible to have two players with the same username. 
+    The username is the only field that is required to create a player. 
+    
+    The is_in_game field is used to check if the player is in a game or not.
+    """
+    username = models.CharField(max_length=20)
+    is_in_game = models.BooleanField(default=False)
+    
+    def __str__(self):
+        return self.username
+    
+    
+class Game(models.Model):
+    players = models.ManyToOneRel(Player, related_name='game')
