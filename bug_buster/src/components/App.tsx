f2f113489aa774251a73ad9cb80d3be6d1be319b
cepg3 <c.e.p.g.power@gmail.com>
2024-03-20 11:59:47 -0400

    Feat : Add Join component and handle game code change

--- bug_buster/src/components/App.tsx
+++ bug_buster/src/components/App.tsx
@@ -5,12 +5,19 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Login from "./Login/Login";
 import PageNotFound from "./PageNotFound";
 import Dashboard from "./Dashboard";
+import Join from "./Join";
 
 function App() {
 	const [user, setUser] = React.useState<UserContextType>({ username: "" });
 	const handleUserChange = (user: UserContextType) => {
 		console.log("User name changed to: ", user);
 		setUser(user);
+	};
+
+	const [gameCode, setGameCode] = React.useState<string>("");
+	const handleGameCodeChange = (gameCode: string) => {
+		console.log("Game code changed to: ", gameCode);
+		setGameCode(gameCode);
 	};
 
 	return (
@@ -36,6 +43,15 @@ function App() {
 					<Route
 						path="/dashboard"
 						element={<Dashboard user={user} />}
+					/>
+					<Route
+						path="/join"
+						element={
+							<Join
+								gameCode={gameCode}
+								onGameCodeChange={handleGameCodeChange}
+							/>
+						}
 					/>
 
 					<Route
