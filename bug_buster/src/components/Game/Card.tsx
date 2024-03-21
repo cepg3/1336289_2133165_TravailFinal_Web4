96a416bbccbe547b6ca121c370e35e655ec33fcb
mickmi121996 <mickmi1996@gmail.com>
2024-03-21 11:44:35 -0400

    Feat : Add the card component

---
+++ bug_buster/src/components/Game/Card.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import { Card as MuiCard, CardContent, Typography } from "@mui/material";
+
+type CardType = 'Bug' | 'User' | 'Start' | 'Middle' | 'End';
+
+const getBorderColor = (type: CardType): string => {
+  switch (type) {
+    case 'Bug':
+      return 'red';
+    case 'User':
+      return 'blue';
+    case 'Start':
+      return 'green';
+    case 'Middle':
+      return 'yellow';
+    case 'End':
+      return 'purple';
+    default:
+      return 'grey';
+  }
+};
+
+export default function Card({ text, type }: { text: string; type: CardType; }) {
+  return (
+    <MuiCard style={{ width: '300px', margin: '20px', border: `2px solid ${getBorderColor(type)}`, display: 'inline-block', transform: 'rotate(-90deg)' }}>
+      <CardContent>
+        <Typography variant="body2" component="p">
+          {text}
+        </Typography>
+      </CardContent>
+    </MuiCard>
+  );
+}
