0f1bf606a141995f4d3fdbd8c3b2b31b7d9a1ea7
cepg3 <2133165@etu.cchic.ca>
2024-03-21 13:22:20 -0400

    Fix : Add keepChecking  to Game.tsx

--- bug_buster/src/components/Game/Game.tsx
+++ bug_buster/src/components/Game/Game.tsx
@@ -6,31 +6,35 @@ export default function Game({
 	gameCode,
 	user,
 	onUserChange,
+	keepChecking,
 	setKeepChecking,
 }: {
 	gameCode: string;
 	user: UserContextType;
 	onUserChange: (user: UserContextType) => void;
+	keepChecking: boolean;
 	setKeepChecking: (keepChecking: boolean) => void;
 }) {
 	useEffect(() => {
 		// Makes sure the user doesn't exist in the database
 		const checkUser = async () => {
-			console.log("Checking username : ", user.username);
+			if (keepChecking) {
+				console.log("Checking username : ", user.username);
 
-			const response = await api.isUsernameTaken(user.username);
-			if (response) {
-				console.log("Username already taken : ", user.username);
-				onUserChange({ username: "", id: null });
-				return;
+				const response = await api.isUsernameTaken(user.username);
+				if (response) {
+					console.log("Username already taken : ", user.username);
+					onUserChange({ username: "", id: null });
+					return;
+				}
+
+				// If the user doesn't exist, creates it
+				const newUser = await api.createPlayer(user.username);
+
+				// Stops checking that the username is taken because the user will have joined a game
+				setKeepChecking(false);
+				console.log("Stopped username verification");
 			}
-
-			// If the user doesn't exist, creates it
-			const newUser = await api.createPlayer(user.username);
-
-			// Stops checking that the username is taken because the user will have joined a game
-			setKeepChecking(false);
-			console.log("Stopped username verification");
 		};
 
 		checkUser();
