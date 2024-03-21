2ddfbc75a6fe9b2d03f96f34b50019d1254ee895
cepg3 <2133165@etu.cchic.ca>
2024-03-21 11:18:50 -0400

    Feat : Add setKeepChecking in Game.tsx

--- bug_buster/src/components/Game/Game.tsx
+++ bug_buster/src/components/Game/Game.tsx
@@ -4,9 +4,11 @@ import { UserContextType } from "../Login/UserContext";
 export default function Game({
 	gameCode,
 	user,
+	setKeepChecking,
 }: {
 	gameCode: string;
 	user: UserContextType;
+	setKeepChecking: (keepChecking: boolean) => void;
 }) {
 	return <div>Game</div>;
 }
