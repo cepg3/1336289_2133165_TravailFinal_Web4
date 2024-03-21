bf5f86e7542488b2a5526c2d5557c4cbc934066c
mickm <mickmi1996@gmail.com>
2024-03-21 15:15:17 -0400

    Feat : add ClientCard.tsx

--- bug_buster/src/components/Game/ClientCard.tsx
+++ bug_buster/src/components/Game/ClientCard.tsx
@@ -1,7 +1,28 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import { Box } from '@mui/material';
+import GameCard from './GameCard';
+import api from '../../Api';
+import { GameCardType } from '../../Api';
 
-export default function ClientCard() {
+export default function ClientCard({ gameId, playerId } : { gameId: number, playerId: number }) {
+  const [cards, setCards] = useState<GameCardType[]>([]);
+
+  useEffect(() => {
+    const fetchPlayerCards = async () => {
+      const playerCards = await api.getPlayerCards(gameId, playerId);
+      setCards(playerCards);
+    };
+
+    fetchPlayerCards();
+  }, [gameId, playerId]);
+
+  const userCard = cards.find(card => card.cardCategory === 'User');
+  const bugCard = cards.find(card => card.cardCategory === 'Bug');
+
   return (
-    <div>ClientCard</div>
-  )
+    <Box display="flex" justifyContent="center" alignItems="center">
+      {userCard && <GameCard gameCardType={userCard} />}
+      {bugCard && <GameCard gameCardType={bugCard} />}
+    </Box>
+  );
 }
