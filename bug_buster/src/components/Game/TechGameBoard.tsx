a04af7d87d22809f0f029bda17f4852ef5e5f173
mickm <mickmi1996@gmail.com>
2024-03-21 15:21:06 -0400

    Feat : add TechGameBoard.tsx

--- bug_buster/src/components/Game/TechGameBoard.tsx
+++ bug_buster/src/components/Game/TechGameBoard.tsx
@@ -1,7 +1,24 @@
-import React from 'react'
+import React from 'react';
+import { Box, Typography } from '@mui/material';
+import ClientCard from './ClientCard';
+import PlayerPoints from './PlayerPoints';
+import CardListDisplay from './CardListDisplay';
 
 export default function TechGameBoard() {
+  // TODO Modifier les valeurs gameId et playerId pour correspondre à votre partie
+  const gameId = Number("123");
+  const playerId = Number("456");
+
   return (
-    <div>TechGameBoard</div>
-  )
+    <Box sx={{ padding: 2 }}>
+      <Typography variant="h4" gutterBottom>Tableau de jeu</Typography>
+      
+      <Box display="flex" justifyContent="space-around" alignItems="center" mb={4}>
+        <ClientCard gameId={gameId} playerId={playerId} />
+        <PlayerPoints gameId={gameId} playerId={playerId} />
+      </Box>
+
+      <CardListDisplay gameId={gameId} playerId={playerId} />
+    </Box>
+  );
 }
