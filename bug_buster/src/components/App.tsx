ae0b0a1cd90d743475eaf4b368d233eea990207a
cepg3 <2133165@etu.cchic.ca>
2024-03-21 11:10:45 -0400

    Feat : Add a verification on every locatino change for the username

--- bug_buster/src/components/App.tsx
+++ bug_buster/src/components/App.tsx
@@ -1,15 +1,60 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "../assets/css/App.css";
 import { UserContextType } from "./Login/UserContext";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes, useLocation } from "react-router-dom";
 import Login from "./Login/Login";
 import PageNotFound from "./PageNotFound";
 import Dashboard from "./Dashboard";
 import Join from "./Join";
 import Create from "./Create";
 import Game from "./Game/Game";
+import api from "../Api";
+import {
+	Button,
+	Dialog,
+	DialogContent,
+	DialogContentText,
+	DialogTitle,
+} from "@mui/material";
 
 function App() {
+	const [showModal, setShowModal] = useState<boolean>(false);
+
+	return (
+		<div
+			style={{
+				display: "flex",
+				justifyContent: "center",
+				alignItems: "center",
+				height: "100vh",
+			}}
+		>
+			<BrowserRouter>
+				<Main setShowModal={setShowModal}/>
+			</BrowserRouter>
+			{showModal ? (
+				<Dialog open={showModal} onClose={() => setShowModal(false)}>
+				<DialogTitle>Nom d'utilisateur déjà pris</DialogTitle>
+				<DialogContent>
+					<DialogContentText gutterBottom>
+                        Veuillez choisir un autre nom d'utilisateur.
+					</DialogContentText>
+					<Button
+						variant="contained"
+						onClick={() => setShowModal(false)}
+					>
+						Fermer
+					</Button>
+				</DialogContent>
+			</Dialog>
+			) : null}
+		</div>
+	);
+}
+
+function Main({setShowModal}: {setShowModal: (show: boolean) => void}) {
+	const location = useLocation();
+
 	const [user, setUser] = React.useState<UserContextType>({ username: "" });
 	const handleUserChange = (user: UserContextType) => {
 		console.log("User name changed to: ", user);
@@ -22,75 +67,72 @@ function App() {
 		setGameCode(gameCode);
 	};
 
-	return (
-		<div
-			style={{
-				display: "flex",
-				justifyContent: "center",
-				alignItems: "center",
-				height: "100vh",
-			}}
-		>
-			<BrowserRouter>
-				{user?.username != null && user?.username !== "" ? (
-					<Routes>
-						<Route
-							path="/"
-							element={
-								<Login
-									user={user}
-									onUserChange={handleUserChange}
-								/>
-							}
-						/>
-						<Route
-							path="/dashboard"
-							element={<Dashboard user={user} />}
-						/>
-						<Route
-							path="/join"
-							element={
-								<Join
-									gameCode={gameCode}
-									onGameCodeChange={handleGameCodeChange}
-								/>
-							}
-						/>
-						<Route
-							path="/create"
-							element={
-								<Create
-									gameCode={gameCode}
-									onGameCodeChange={handleGameCodeChange}
-								/>
-							}
-						/>
-						<Route
-							path="/game"
-							element={<Game gameCode={gameCode} user={user} />}
-						/>
+	useEffect(() => {
+		const checkUser = async () => {
+			console.log("Checking username : ", user.username);
 
-						<Route
-							path="*"
-							element={
-								user?.username != null &&
-								user?.username !== "" ? (
-									<PageNotFound />
-								) : (
-									<Login
-										user={user}
-										onUserChange={handleUserChange}
-									/>
-								)
-							}
+			const response = await api.isUsernameTaken(user.username);
+			if (response) {
+				console.log("Username already taken : ", user.username);
+				setShowModal(true);
+				handleUserChange({ username: "" });
+			}
+		};
+
+		checkUser();
+	}, [user, location]);
+
+	if (user?.username != null && user?.username !== "") {
+		return (
+			<Routes>
+				<Route
+					path="/"
+					element={
+						<Login user={user} onUserChange={handleUserChange} />
+					}
+				/>
+				<Route path="/dashboard" element={<Dashboard user={user} />} />
+				<Route
+					path="/join"
+					element={
+						<Join
+							gameCode={gameCode}
+							onGameCodeChange={handleGameCodeChange}
 						/>
-					</Routes>
-				) : (
-					<Login user={user} onUserChange={handleUserChange} />
-				)}
-			</BrowserRouter>
-		</div>
-	);
+					}
+				/>
+				<Route
+					path="/create"
+					element={
+						<Create
+							gameCode={gameCode}
+							onGameCodeChange={handleGameCodeChange}
+						/>
+					}
+				/>
+				<Route
+					path="/game"
+					element={<Game gameCode={gameCode} user={user} />}
+				/>
+
+				<Route
+					path="*"
+					element={
+						user?.username != null && user?.username !== "" ? (
+							<PageNotFound />
+						) : (
+							<Login
+								user={user}
+								onUserChange={handleUserChange}
+							/>
+						)
+					}
+				/>
+			</Routes>
+		);
+	} else {
+		return <Login user={user} onUserChange={handleUserChange} />;
+	}
 }
 
 export default App;
