0bb8630548911fa10b0615386a4ea44322cd59df
mickmi121996 <mickmi1996@gmail.com>
2024-03-21 13:28:31 -0400

    Feat : Add the GameCardList.tsx to contain a GameClientRadioButton.tsx

---
+++ bug_buster/src/components/Game/GameCardList.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { Typography, Box } from "@mui/material";
+import GameCardRadioButton from "./GameCardRadioButton";
+import { GameCardType } from "../../Api";
+
+export default function GameCardList({
+  cards,
+}: {
+  cards: GameCardType[];
+}) {
+  return (
+    <Box sx={{ margin: 2 }}>
+      <Typography variant="h5" gutterBottom>
+        {cards[0].cardCategory}
+      </Typography>
+      {cards.map((card) => (
+        <GameCardRadioButton key={card.id} gameCardType={card} />
+      ))}
+    </Box>
+  );
+}
