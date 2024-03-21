9a56b6a9b52fa56c7385f069554415449f2c3edf
mickm <mickmi1996@gmail.com>
2024-03-21 16:46:39 -0400

    sdasfdsaf

--- bug_buster/src/components/Game/Game.tsx
+++ bug_buster/src/components/Game/Game.tsx
@@ -1,101 +1,102 @@
 import React, { useEffect, useRef } from "react";
 import { UserContextType } from "../Login/UserContext";
+import { CircularProgress, Box } from "@mui/material";
 import api, { GameType, PlayerType } from "../../Api";
+import TechGameBoard from "./TechGameBoard";
+import ClientGameBoard from "./ClientGameBoard";
 
 export default function Game({
-	gameCode,
-	user,
-	setUser,
-	keepChecking,
-	setKeepChecking,
+  gameCode,
+  user,
+  setUser,
+  keepChecking,
+  setKeepChecking,
 }: {
-	gameCode: string;
-	user: UserContextType;
-	setUser: (user: UserContextType) => void;
-	keepChecking: boolean;
-	setKeepChecking: (keepChecking: boolean) => void;
+  gameCode: string;
+  user: UserContextType;
+  setUser: (user: UserContextType) => void;
+  keepChecking: boolean;
+  setKeepChecking: (keepChecking: boolean) => void;
 }) {
-	const [game, setGame] = React.useState<GameType | null>(null);
+  const [game, setGame] = React.useState<GameType | null>(null);
 
-	useEffect(() => {
-		// Makes sure the user doesn't exist in the database
-		const checkUser = async () => {
-			if (keepChecking) {
-				console.log("Checking username : ", user.username);
+  useEffect(() => {
+    // Makes sure the user doesn't exist in the database
+    const checkUser = async () => {
+      if (keepChecking) {
+        console.log("Checking username : ", user.username);
 
-				setGame(
-					await api
-						.isUsernameTaken(user.username)
-						.then(async (response) => {
-							if (response) {
-								console.log(
-									"Username already taken : ",
-									user.username
-								);
-								return true;
-							} else {
-								const newUser = await api.createPlayer(
-									user.username
-								);
-								setUser({
-									username: newUser.username,
-									id: newUser.id,
-									is_in_game: true,
-									is_client: true,
-								});
-								console.log("User created : ", user.username);
-								return newUser;
-							}
-						})
-						.then(async (userOrResponse) => {
-							if (userOrResponse === true) {
-								return await api.getPlayer(user.username);
-							} else {
-								return userOrResponse;
-							}
-						})
-						.then(async (user) => {
-							// Makes sure the game exists
-							return await api
-								.doesGameExist(gameCode)
-								.then(async (gameExist) => {
-									if (gameExist) {
-										return await api.getGame(gameCode);
-									} else {
-										console.log(
-											"Creating game : ",
-											gameCode
-										);
-										return await api
-											.createGame(gameCode)
-											.then((newGame) => {
-												if (newGame.id != null) {
-													console.log(
-														"Game created : ",
-														gameCode
-													);
-													return newGame;
-												}
-												return null;
-											});
-									}
-								})
-								.then(async (nullOrNewGame) => {
-									if (nullOrNewGame != null) {
-										return await api.joinGame(
-											nullOrNewGame.id,
-											user.id
-										);
-									}
-									return null;
-								});
-						})
-				);
-			}
-		};
+        setGame(
+          await api
+            .isUsernameTaken(user.username)
+            .then(async (response) => {
+              if (response) {
+                console.log("Username already taken : ", user.username);
+                return true;
+              } else {
+                const newUser = await api.createPlayer(user.username);
+                setUser({
+                  username: newUser.username,
+                  id: newUser.id,
+                  is_in_game: true,
+                  is_client: true,
+                });
+                console.log("User created : ", user.username);
+                return newUser;
+              }
+            })
+            .then(async (userOrResponse) => {
+              if (userOrResponse === true) {
+                return await api.getPlayer(user.username);
+              } else {
+                return userOrResponse;
+              }
+            })
+            .then(async (user) => {
+              // Makes sure the game exists
+              return await api
+                .doesGameExist(gameCode)
+                .then(async (gameExist) => {
+                  if (gameExist) {
+                    return await api.getGame(gameCode);
+                  } else {
+                    console.log("Creating game : ", gameCode);
+                    return await api.createGame(gameCode).then((newGame) => {
+                      if (newGame.id != null) {
+                        console.log("Game created : ", gameCode);
+                        return newGame;
+                      }
+                      return null;
+                    });
+                  }
+                })
+                .then(async (nullOrNewGame) => {
+                  if (nullOrNewGame != null) {
+                    return await api.joinGame(nullOrNewGame.id, user.id);
+                  }
+                  return null;
+                });
+            })
+        );
+      }
+    };
 
-		checkUser();
-	}, [user, keepChecking, setKeepChecking, setUser, gameCode]);
+    checkUser();
+  }, [user, keepChecking, setKeepChecking, setUser, gameCode]);
 
-	return <div>Game</div>;
+  if (!game) {
+    return <div>Chargement</div>;
+  } else {
+    const isClient = user.is_client;
+
+    return isClient ? (
+      <ClientGameBoard gameId={game.id} playerId={user.id!} />
+    ) : (
+      <TechGameBoard
+        gameId={game.id}
+        playerId={user.id!}
+        clientId={game.current_client_player_id}
+      />
+    );
+  }
 }
